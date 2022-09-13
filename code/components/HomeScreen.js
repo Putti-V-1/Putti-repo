c297b9e4d9a8e97c2881bce9d2a8ae587c9797f5
Anton <antonbe20@tskoli.is>
2022-09-13 13:09:25 +0000

    upprunalegt mapView komid

--- code/components/HomeScreen.js
+++ code/components/HomeScreen.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Kort</Text>
-    </View>
+    <View style={styles.container}>
+    <MapView style={styles.map} />
+  </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
