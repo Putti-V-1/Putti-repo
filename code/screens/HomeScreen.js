6054e89313b0085fe98c9837a9fbb8f2e425b2e9
Nussari <viktorandri17@gmail.com>
2022-09-22 08:59:19 +0000

    Notification skjar og dev login

--- code/screens/HomeScreen.js
+++ code/screens/HomeScreen.js
@@ -7,35 +7,11 @@ import {WatchScreen} from '../components/WatchScreen';
 import {BookedScreen} from '../components/BookedScreen';
 import CustomDrawer from'../components/Drawer';
 import {ProfileScreen} from '../components/ProfileScreen';
-import { auth } from '../firebase'
-import { useNavigation } from '@react-navigation/core'
+import { useNavigation } from '@react-navigation/core';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
-const headerOptions = {
-  headerRight: () => (
-    <Button
-      onPress={() => {
-        auth
-          .signOut()
-          .then(() => {
-              navigation.navigate('Login');
-          })
-          .catch(error => alert(error.message))
-      }}
-      title="Logout"
-      color="#fff"
-    />
-  ),
-  headerStyle: {
-    backgroundColor: 'gray',
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    fontWeight: 'bold',
-  },
-}
 
 function TabNav(){
   return(
@@ -47,15 +23,33 @@ function TabNav(){
   );
 }
 
-const HomeScreen = () => {
-    return (
-      <NavigationContainer independent={true} drawerContent={props => <CustomDrawer {...props} />}>
-        <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
-          <Drawer.Screen name="Tabs" component={TabNav} />
-          <Drawer.Screen name="Profile" component={ProfileScreen} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
+const HomeScreen = ({route}) => {
+  const tabNav = route.params.stackNav;
+  return (
+    <NavigationContainer independent={true} drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />} screenOptions={() => ({
+        headerRight: () => (
+          <Button
+            onPress={()=>{tabNav.navigate('Notifications')}}
+            title="Notif"
+            color="#fff"
+          />
+        ),
+        headerStyle: {
+          backgroundColor: 'gray',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      })
+
+      }>
+        <Drawer.Screen name="Tabs" component={TabNav} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default HomeScreen
---
+++ code/screens/NotificationScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+const NotificationScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications</Text>
+    </View>
+  );
+}
+
+export default NotificationScreen;
