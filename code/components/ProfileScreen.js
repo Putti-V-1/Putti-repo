940880607682db4e82ef1aabdac4bdc19f970168
Nussari <viktorandri17@gmail.com>
2022-09-27 14:12:27 +0000

    driver bool og plan skjar bætt, nav error lagaður

--- code/components/ProfileScreen.js
+++ code/components/ProfileScreen.js
@@ -4,16 +4,9 @@ import { ref, get, child } from 'firebase/database';
 
 
 export function ProfileScreen() {
-  get(child(ref(global.db), `users/${global.user["uid"]}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      const user = snapshot.val();
-    }else{
-      const user = {}
-    }
-  })
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{user["email"]}</Text>
+      <Text>{global.user["email"]}</Text>
     </View>
   );
 }
