99d59a33e11face2f661678ee44565b822d8e0cc
Nussari <viktorandri17@gmail.com>
2022-10-13 10:07:46 +0000

    booked og watch

--- code/screens/BookedScreen.js
+++ code/screens/BookedScreen.js
@@ -1,10 +1,29 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
+
+function renderRide(ride){
+  return (
+    <TouchableOpacity style={{borderWidth: "1px", marginTop: 12, borderRadius: 10, width: "85%", padding: 5}}>
+      <Text>
+        From: {ride.child("origin").child("address").val()}
+      </Text>
+      <Text>
+        To: {ride.child("destination").child("address").val()}
+      </Text>
+      <Text>
+        Time: {ride.child("time").val()}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export function BookedScreen() {
+  console.log(global.bookedRides)
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Bókaðar ferðir</Text>
-    </View>
+    <ScrollView contentContainerStyle={{flex: 1}}>
+      <View style={{flex: 1, alignItems: "center"}}>
+        {global.allRides.map((ride) => {return renderRide(ride)}) /*breyti seinna*/}
+      </View>
+    </ScrollView>
   );
 }
